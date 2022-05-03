3b4f391831ceca4d83b0d6eb04565ddacd8921c6
pnoronha <pnoronha@student.42lisboa.com>
2022-05-03 14:56:11 +0100

    adding fixed number of arguments to mandelbrot at input checker

--- err_ret.c
+++ err_ret.c
@@ -6,7 +6,7 @@
 /*   By: pnoronha <pnoronha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 18:39:41 by pnoronha          #+#    #+#             */
-/*   Updated: 2022/05/02 20:08:55 by pnoronha         ###   ########.fr       */
+/*   Updated: 2022/05/03 14:28:11 by pnoronha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	print_input(void)
 	ft_printf("Error: Wrong program usage. \n\n\
 	Try something like:\n\
 		-> ./fractol mandelbrot\n\
-		-> ./fractol julia (number) (number)\n");
+		-> ./fractol julia 0.4197 -0.3219\n");
 	exit(EXIT_FAILURE);
 }
 
@@ -25,7 +25,7 @@ void	check_input(char **argv, int argc)
 {
 	if (argc < 2)
 		print_input();
-	if (ft_strncmp(*argv, "mandelbrot\0", 11) == 0)
+	else if (ft_strncmp(*argv, "mandelbrot\0", 11) == 0 && argc == 2)
 		base()->fractol_type = mandelbrot;
 	else if (ft_strncmp(*argv, "julia\0", 6) == 0)
 	{
